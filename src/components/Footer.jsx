cb33e47bf41dc4411218e0c32691f8f2872c6532
Aguero15 <nazarenoaguero65@gmail.com>
2021-11-30 23:51:56 -0300

    falta terminar

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+function Footer(){
+    return (
+<div>
+<footer class="sticky-footer bg-white">
+				<div class="container my-auto">
+					<div class="copyright text-center my-auto">
+						<span>Copyright © Dashboard 2021</span>
+					</div>
+				</div>
+			</footer>
+</div>
+    )
+}
+export default Footer;
